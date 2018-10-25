2e16b17e747bb9771192d66a67295c5adfd7b597
raulignacio710 <37843005+raulignacio710@users.noreply.github.com>
2018-10-25 13:37:15 -0300

    Maquetacion en proceso

---
+++ instrumentalstore/sistema/static/js/carrusel.js
@@ -0,0 +1,7 @@
+// Cuando se termine de cargar la página se ejecutará este bloque para inicializar el carrusel
+$(document).ready(function() {
+	$("#carrusel").bxSlider({
+		mode: "horizontal",
+		auto: true
+	});
+});
