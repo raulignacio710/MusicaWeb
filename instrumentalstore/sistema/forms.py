d98ec15f2bde4f876d4b91e34fec5132774ce4a0
Takumi666 <Takumi666@users.noreply.github.com>
2018-11-08 19:32:41 -0300

    Modelos y Formularios
    Añadido todas las entidades posibles del modelo, y gran parte del formulario planeado :v

---
+++ instrumentalstore/sistema/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+# Formularios
+class FormContacto(forms.Form):
+    nombre = forms.CharField(label = "Nombre", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese nombre" }))
+    correo = forms.EmailField(label = "Correo electróncio", max_length = 30, widget = forms.EmailInput(attrs = { "placeholder": "direccion@correo.com" }))
+    mensaje = forms.CharField(label = "Mensaje", widget = forms.Textarea(attrs = { "placeholder": "Ingrese mensaje" }))
+
+class FormCliente(forms.Form):
+    runCliente = forms.CharField(label = "RUN cliente", max_length = 10, widget = forms.TextInput(attrs = { "placeholder": "12345678-9" }))
+    nombres = forms.CharField(label = "Nombres", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese nombres" }))
+    apPaterno = forms.CharField(label = "Apellido paterno", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese apellido paterno"}))
+    apMaterno = forms.CharField(label = "Apellido materno", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese apellido materno"}))
+
+class FormInstrumento(forms.Form):
+    marca = forms.CharField(label = "Marca", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese marca" }))
+    modelo = forms.CharField(label = "Modelo", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese modelo" }))
+    serie = forms.CharField(label = "Serie", max_length = 15, widget = forms.TextInput(attrs = { "placeholder": "Ingrese serie" }))
+    tipo = forms.CharField(label = "Tipo", max_length = 20, widget = forms.TextInput(attrs = { "placeholder": "Ingrese tipo" }))
+    descripcion = forms.CharField(label = "Descripcion", widget = forms.Textarea(attrs = { "placeholder": "Ingrese descripción" }))
+    precio = forms.IntegerField(label = "Precio", widget = forms.NumberInput(attrs = { "min": "0" }))
+    stock = forms.IntegerField(label = "Stock", widget = forms.NumberInput(attrs = { "min": "0" }))
--- instrumentalstore/sistema/models.py
+++ instrumentalstore/sistema/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Mensaje(models.Model):
+    codigoMensaje = models.AutoField(primary_key = True)
+    nombreRemitente = models.CharField(max_length = 20)
+    correo = models.EmailField(max_length = 30)
+    mensaje = models.TextField()
+
+class Cliente(models.Model):
+    codigoCliente = models.AutoField(primary_key = True)
+    run = models.CharField(max_length = 10)
+    nombres = models.CharField(max_length = 20)
+    apPaterno = models.CharField(max_length = 20)
+    apMaterno = models.CharField(max_length = 20)
+
+class Instrumento(models.Model):
+    codigoInstrumento = models.AutoField(primary_key = True)
+    marca = models.CharField(max_length = 20)
+    modelo = models.CharField(max_length = 20)
+    serie = models.CharField(max_length = 15)
+    tipo = models.CharField(max_length = 20)
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+class Venta(models.Model):
+    codigoVenta = models.AutoField(primary_key = True)
+    cliente = models.ForeignKey(Cliente, on_delete = models.DO_NOTHING)
+    instrumento = models.ForeignKey(Instrumento, on_delete = models.DO_NOTHING)
+    fecha = models.DateField()
